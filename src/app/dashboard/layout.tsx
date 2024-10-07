0da06d3314f163e2e34d448ba2e672d8ce9fcbd8
Majo <olaecheamariajose@gmail.com>
2024-10-07 10:22:26 -0300

    Feat. Sidebar + icons

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -6,10 +6,9 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="bg-slate-100 overflow-y-scroll w-screen h-screen antialiased text-slate-300 selection:bg-blue-600 selection:text-white">
-      <div className="flex flex-col relative w-screen">
-        <Sidebar />
-      </div>
-    </div>
+    <>
+      <Sidebar />
+      <main className="flex flex-col items-center p-24">{children}</main>
+    </>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 import { redirect } from "next/navigation";
 
 export default function HomePage() {
-  redirect("dashboard/counter");
+  redirect("dashboard/main");
   return <h1>Hola</h1>;
 }
