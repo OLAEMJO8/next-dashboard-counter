35559814cde8c4c1b12e44e242e82d33e646861a
Majo <olaecheamariajose@gmail.com>
2024-10-07 10:49:18 -0300

    Feat. card counter

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,30 +1,14 @@
-"use client";
+import { CardCounter } from "@/app/shopping-cart/components";
 
-import { useState } from "react";
+export const metadata = {
+  title: "Counter Page",
+  description: "Un simple contador",
+};
 export default function CounterPage() {
-  const [count, setCount] = useState(5);
-
   return (
     <div className="flex flex-col items-center justify-center w-full h-screen pl-64">
       <span>Productos en el carrito</span>
-      <span className="text-9xl">{count}</span>
-
-      <div className="mt-4 space-x-4">
-        <button
-          onClick={() => setCount(count + 1)}
-          className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none rounded-full"
-          type="button"
-        >
-          +1
-        </button>
-        <button
-          onClick={() => setCount(count - 1)}
-          className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 border border-gray-900 text-gray-900 hover:opacity-75 focus:ring focus:ring-gray-300 active:opacity-[0.85] rounded-full"
-          type="button"
-        >
-          -1
-        </button>
-      </div>
+      <CardCounter />
     </div>
   );
 }
