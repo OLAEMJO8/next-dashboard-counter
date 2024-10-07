1740dc5a1735883fb73f034e77c72dc862b20629
Majo <olaecheamariajose@gmail.com>
2024-10-07 11:12:00 -0300

    Edit

--- src/app/dashboard/main/page.tsx
+++ src/app/dashboard/main/page.tsx
@@ -1,3 +1,14 @@
 export default function MainPage() {
-  return <div>Main </div>;
+  return (
+    <div className="flex flex-col items-center justify-center w-full">
+      <div className="lg:2/6 xl:w-2/4 mt-20 lg:mt-40 lg:ml-16 text-left">
+        <div className="text-6xl font-semibold text-gray-900 leading-none">
+          Bienvenidos al contador con Next.js
+        </div>
+        <div className="mt-6 text-xl font-light text-true-gray-500 antialiased">
+          Por la Navbar podes acceder al contador
+        </div>
+      </div>
+    </div>
+  );
 }
